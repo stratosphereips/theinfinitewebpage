534318fdec4a425e567c06c22fbcef4f531ccc2e
Veronica Valeros <vero.valeros@gmail.com>
2023-08-21 10:53:23 +0200

    Add closing tag to html

--- lib/custom_http.py
+++ lib/custom_http.py
@@ -1,4 +1,6 @@
 def generate_html():
     doctype = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\"><html><head><title>This is a TL;DR page.</title></head><body>"
     content = "What you are looking for is in the next line<br>" * 100
-    return doctype + content
+    closing_tag = "</body></html>"
+
+    return doctype + content + closing_tag
